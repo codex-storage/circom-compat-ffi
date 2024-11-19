297c46fdc7d8a8fd53c8076b0be77334e4a54447
markoburcul <marko@status.im>
2024-11-19 11:15:35 +0100

    nix: added flake and derivation for the project
    Signed-off-by: markoburcul <marko@status.im>

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "A flake for building circom-compat (ark-circom) ffi";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  };
+
+  outputs = { self, nixpkgs }: 
+    let
+      stableSystems = [
+        "x86_64-linux" "aarch64-linux"
+        "x86_64-darwin" "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs stableSystems;
+      pkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in
+    {
+      packages = forAllSystems (system: let
+        pkgs = pkgsFor.${system};
+      in {
+        default = pkgs.callPackage ./default.nix {};
+      });
+    };
+}
